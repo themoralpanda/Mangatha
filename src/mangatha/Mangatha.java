fc97fd6e6a9a677fb84309b81cc590beb951ef3d
Vigneshwar V <haivicky@gmail.com>
2013-07-08 16:47:31 +0530

    initial commit
    project folder added

---
+++ src/mangatha/Mangatha.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mangatha;
+
+import com.Mangatha.ui.PlayDesk;
+
+/**
+ *
+ * @author Pathi
+ */
+public class Mangatha {
+
+    //String nos[] ={"A","1","2","3","4","5","6","7","8","9","10","J","Q","K" } ;
+    //ArrayList<String> deck = new ArrayList<String>();
+   // public void init()
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("vicky is a good boy");
+        PlayDesk obj = new PlayDesk();
+        
+        obj.setVisible(true);
+    }
+}
